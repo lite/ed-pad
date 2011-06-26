a6666c3a0d95b8db7c5eca4a334bea38f3607df9
lite <litekok@gmail.com>
2011-06-27 01:25:09 +0800

    first commit

---
+++ ed-pad/ed_padAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ed_padAppDelegate.h
+//  ed-pad
+//
+//  Created by dennisli on 6/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ed_padAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ed-pad/iPhone/ed_padAppDelegate_iPhone.m
@@ -0,0 +1,18 @@
+//
+//  ed_padAppDelegate_iPhone.m
+//  ed-pad
+//
+//  Created by dennisli on 6/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ed_padAppDelegate_iPhone.h"
+
+@implementation ed_padAppDelegate_iPhone
+
+- (void)dealloc
+{
+	[super dealloc];
+}
+
+@end
